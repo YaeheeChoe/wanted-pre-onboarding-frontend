521a0e2f1d75c2419fb77c65b9fc00b9dde05201
Yaehee Choe <yaehee@kakao.com>
2023-06-16 21:24:55 +0900

    add blank space

--- src/component/EmailInput.js
+++ src/component/EmailInput.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const RootCont = styled.div`
     display:flex;
     justify-content: center;
+    margin: 8px;
 `
 const Input = styled.input`
     width: 200px;
--- src/page/Signin.js
+++ src/page/Signin.js
@@ -3,14 +3,15 @@ import EmailInput from '../component/EmailInput'
 import PasswordInput from '../component/PasswordInput'
 import styled from 'styled-components'
 const RootCont =styled.div`
-    width:100%;
-    height:100%;
-`
-const Cont = styled.div`
     position: absolute;
     width :300px;
+    height: 300px;
     top: calc(50% - 100px);
     left: calc(50% - 150px);
+`
+const Cont = styled.div`
+    display:flex;
+    flex-direction:column;
 `;
 
 
