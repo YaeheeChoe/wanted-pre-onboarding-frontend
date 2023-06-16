5e65f6fd6288251da91e3958f59526500354887e
Yaehee Choe <yaehee@kakao.com>
2023-06-16 18:54:06 +0900

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
-
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Signin from './page/Signin';
+import Signup from './page/Signup';
+import Todo from './page/Todo';
+import Main from './page/Main';
+import NotFound from './page/NotFound';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path='/' element={<Main/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/signin' element={<Signin/>}></Route>
+          <Route path='/todo' element={<Todo/>}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/page/Main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom';
+
+function Main() {
+    return <Navigate to="/signin"></Navigate>
+}
+
+export default Main
