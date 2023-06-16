8115d10b3108ac151bf78e59b4f55b3a8fef005e
Yaehee Choe <yaehee@kakao.com>
2023-06-16 22:01:25 +0900

    add validatePassword

--- src/component/PasswordInput.js
+++ src/component/PasswordInput.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useState } from 'react'
 
 const RootCont = styled.div`
-    display:flex;
-    justify-content: center;
     margin: 8px;
 `
 const Input = styled.input`
     width: 200px;
 `
+const Label = styled.label`
+    display:block;
+    text-align:right;
+    font-size: 12px;
+`
 function PasswordInput() {
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const validatePassword = (password) => {
+        const regex = /^.{8,}$/;
+        return regex.test(password);
+    };
+
+    const handleChange = (e) => {
+        const newPassword = e.target.value;
+        setPassword(newPassword);
+
+        if (validatePassword(newPassword)) {
+        setError('');
+        } else {
+        setError('올바른 패스워드 폼을 입력하세요');
+        }
+    };
   return (
     <RootCont>
-        <Input data-testid="password-input" placeholder='password' />
+        <Input data-testid="password-input" type="password" value={password} onChange={handleChange} placeholder='password' />
+        {error && <Label>{error}</Label>}
     </RootCont>
   )
 }
